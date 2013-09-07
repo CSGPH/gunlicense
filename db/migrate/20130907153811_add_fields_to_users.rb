252c24c6b078243d2594549bd7c8195ea69dd594
Vincent Paca <vincent.paca@gmail.com>
2013-09-07 23:42:45 +0800

    Add details to users

---
+++ db/migrate/20130907153811_add_fields_to_users.rb
@@ -0,0 +1,7 @@
+class AddFieldsToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :name, :string
+    add_column :users, :address, :string
+    add_column :users, :contact_number, :string
+  end
+end
