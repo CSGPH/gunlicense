52f8d1882a5670e9dc8d96685cd4ee268a017500
Vincent Paca <vincent.paca@gmail.com>
2013-09-08 00:11:33 +0800

    Add username for users:

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130907153811) do
+ActiveRecord::Schema.define(version: 20130907154739) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -29,9 +29,11 @@ ActiveRecord::Schema.define(version: 20130907153811) do
     t.string   "name"
     t.string   "address"
     t.string   "contact_number"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
 end
