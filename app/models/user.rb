1b12318c8ee7cc84b199bb537a484e732841459c
Vincent Paca <vincent.paca@gmail.com>
2013-09-08 15:53:40 +0800

    Added fix for strong params

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,14 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:login]
+         :recoverable, :rememberable, :trackable, :validatable
 
   validates :username,
     :uniqueness => {
       :case_sensitive => false
     }
+
+  attr_accessor :login
 
   def self.find_first_by_auth_conditions(warden_conditions)
     conditions = warden_conditions.dup
