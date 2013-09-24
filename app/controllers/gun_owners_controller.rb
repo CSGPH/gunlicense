a8c1e7705817c6a1a1511d56b17c5562834f0264
Harvey Enrile <harvey.enrile@gmail.com>
2013-09-24 15:40:00 +0800

    updates for edit gun owner

--- app/controllers/gun_owners_controller.rb
+++ app/controllers/gun_owners_controller.rb
@@ -1,7 +1,17 @@
 class GunOwnersController < ApplicationController
+
   def show
     @gun_owner = GunOwner.find(params[:id])
     @guns = @gun_owner.guns
   end
 
+  def edit
+    @gun_owner = GunOwner.find(params[:id])
+  end
+  def update
+    @gun_owner = GunOwner.find(params[:id])
+    @gun_owner.update_attributes(params[:gun_owner])
+  end
+
+  
 end
