b64b26427c8de21b7c42c4b9d5fb80e2fb2df694
Vincent Paca <vincent.paca@gmail.com>
2013-09-24 15:48:54 +0800

    Fix gun owner update method to use strong parameters

--- app/controllers/gun_owners_controller.rb
+++ app/controllers/gun_owners_controller.rb
@@ -1,5 +1,4 @@
 class GunOwnersController < ApplicationController
-
   def show
     @gun_owner = GunOwner.find(params[:id])
     @guns = @gun_owner.guns
@@ -8,10 +7,15 @@ class GunOwnersController < ApplicationController
   def edit
     @gun_owner = GunOwner.find(params[:id])
   end
+  
   def update
     @gun_owner = GunOwner.find(params[:id])
-    @gun_owner.update_attributes(params[:gun_owner])
+    @gun_owner.update_attributes(gun_owner_params)
   end
-
   
+  private
+  
+  def gun_owner_params
+    params.required(:gun_owner).permit(:name, :address, :city, :province, :region)
+  end
 end
