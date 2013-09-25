864f4e65699b25b6559215d8af5bf4f260e4c27c
Vincent Paca <vincent.paca@gmail.com>
2013-09-25 13:30:29 +0800

    Show gun owner map on single view

--- app/controllers/gun_owners_controller.rb
+++ app/controllers/gun_owners_controller.rb
@@ -2,19 +2,20 @@ class GunOwnersController < ApplicationController
   def show
     @gun_owner = GunOwner.find(params[:id])
     @guns = @gun_owner.guns
+    @json = @gun_owner.to_gmaps4rails
   end
 
   def edit
     @gun_owner = GunOwner.find(params[:id])
   end
-  
+
   def update
     @gun_owner = GunOwner.find(params[:id])
     @gun_owner.update_attributes(gun_owner_params)
   end
-  
+
   private
-  
+
   def gun_owner_params
     params.required(:gun_owner).permit(:name, :address, :city, :province, :region)
   end
