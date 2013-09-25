06d6fcf124fd4cd12e0773b2f7c45e6ea1a27e2e
Vincent Paca <vincent.paca@gmail.com>
2013-09-25 13:24:33 +0800

    Add expired licenses

--- app/models/gun.rb
+++ app/models/gun.rb
@@ -1,3 +1,15 @@
 class Gun < ActiveRecord::Base
   belongs_to :gun_owner, :dependent => :delete
+
+  def self.expired
+    where("expiry_date < ?", Date.today)
+  end
+
+  def expired?
+    if expiry_date < Date.today
+      true
+    else
+      false
+    end
+  end
 end
