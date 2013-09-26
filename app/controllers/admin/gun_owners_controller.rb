2307021843ac7210ba77e4223bed94d2856a37d4
Harvey Enrile <harvey.enrile@gmail.com>
2013-09-26 13:19:35 +0800

    updated edit gun owner

--- app/controllers/admin/gun_owners_controller.rb
+++ app/controllers/admin/gun_owners_controller.rb
@@ -11,10 +11,35 @@ class Admin::GunOwnersController < ApplicationController
     @gun_owner = GunOwner.find(params[:id])
   end
 
+
   def update
+
     @gun_owner = GunOwner.find(params[:id])
-    @gun_owner.update_attributes(gun_owner_params)
+    if @gun_owner.update_attributes(gun_owner_params)
+      redirect_to @gun_owner, notice: 'Updated Succssfully'
+    else
+      render action: "edit"
+    end
+
+
+
   end
+
+  # def update
+  #   @status = current_user.statuses.find(params[:id])
+  #   if params[:status] && params[:status].has_key?(:user_id) 
+  #     params[:status].delete(:user_id) 
+  #   end
+  #   respond_to do |format|
+  #     if @status.update_attributes(params[:status])
+  #       format.html { redirect_to @status, notice: 'Status was successfully updated.' }
+  #       format.json { head :no_content }
+  #     else
+  #       format.html { render action: "edit" }
+  #       format.json { render json: @status.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   private
 
